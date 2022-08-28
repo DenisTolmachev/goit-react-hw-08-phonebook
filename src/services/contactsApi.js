827bbf5b94cef0d5d28ebd2af372ca7de2cd5d91
Denis <denis.tolmachev@gmail.com>
2022-08-28 16:20:42 +0300

    create user Api

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -3,15 +3,6 @@ import { BASE_URL } from 'constants/constants';
 
 axios.defaults.baseURL = BASE_URL;
 
-export const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  }
-};
-
 export const getContacts = async () => {
   return await axios.get(`/contacts`).then(response => response.data);
 };
