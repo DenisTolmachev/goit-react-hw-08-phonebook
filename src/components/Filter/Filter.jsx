508839c2553aab33afffd190ae61f7b5d8b2984d
Denis <denis.tolmachev@gmail.com>
2022-09-10 18:52:50 +0300

    added rendering of the filter by condition

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,25 +1,39 @@
-import { FilterLabelTitle, FilterFormLabel, FilteInput } from './Filter.styled';
+import {
+  FilterLabelTitle,
+  FilterFormLabel,
+  FilteInput,
+  FilterNoneMessage,
+} from './Filter.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { filteredContacts } from 'store/contacts/contactsActions';
-import { getFilter } from 'store/contacts/contactsSelectors';
+import { getFilter, getItems } from 'store/contacts/contactsSelectors';
 
 export const Filter = () => {
   const filter = useSelector(getFilter);
+  const items = useSelector(getItems);
   const dispatch = useDispatch();
 
   const handleFilter = e => {
     dispatch(filteredContacts(e.target.value));
   };
 
+  console.log(items.length);
+
   return (
-    <FilterFormLabel>
-      <FilterLabelTitle>Find contacts by name:</FilterLabelTitle>
-      <FilteInput
-        type="text"
-        placeholder="Enter name"
-        value={filter}
-        onChange={handleFilter}
-      />
-    </FilterFormLabel>
+    <>
+      {items.length === 0 ? (
+        <FilterNoneMessage>Please, add contact!</FilterNoneMessage>
+      ) : (
+        <FilterFormLabel>
+          <FilterLabelTitle>Find contacts by name:</FilterLabelTitle>
+          <FilteInput
+            type="text"
+            placeholder="Enter name"
+            value={filter}
+            onChange={handleFilter}
+          />
+        </FilterFormLabel>
+      )}
+    </>
   );
 };
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -23,3 +23,8 @@ export const FilteInput = styled.input`
   box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px,
     rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
 `;
+
+export const FilterNoneMessage = styled.div`
+  font-size: ${({ theme }) => theme.fontSizes.ml};
+  color: ${({ theme }) => theme.colors.secondary};
+`;
